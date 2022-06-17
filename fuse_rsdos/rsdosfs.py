a3c35dd48a79ef1844015f1b0ab94b5871d9f1b1
Darsey Litzenberger <dlitz@dlitz.net>
2022-06-17 06:27:06 -0700

    Move geometry into Geometry class

--- fuse_rsdos/rsdosfs.py
+++ fuse_rsdos/rsdosfs.py
@@ -38,19 +38,28 @@ from .encodings import tandycoco_hires
 # * 256 bytes/sector
 # = 446,976 bytes
 
-sectors_per_track = 18
-sectors_per_granule = 9
-bytes_per_sector = 256
-directory_track = 17
-#directory_track = 34
-directory_sectors = 9   # Sectors #3 to #11
-max_granules = 192
+class Geometry:
+    sectors_per_track = 18
+    sectors_per_granule = 9
+    bytes_per_sector = 256
+    directory_track = 17
+    #directory_track = 34
+    directory_sectors = 9   # Sectors #3 to #11
+    max_granules = 192
+    directory_entry_size = 32
+    sides = 1
 
-max_filesystem_sectors = sectors_per_granule * max_granules + sectors_per_track
-max_filesystem_bytes = max_filesystem_sectors * bytes_per_sector
+    @property
+    def max_filesystem_sectors(self):
+        return self.sectors_per_granule * self.max_granules + self.sectors_per_track
 
-directory_entry_size = 32
-max_directory_entries = directory_sectors * bytes_per_sector // directory_entry_size
+    @property
+    def max_filesystem_bytes(self):
+        return self.max_filesystem_sectors * self.bytes_per_sector
+
+    @property
+    def max_directory_entries(self):
+        return self.directory_sectors * self.bytes_per_sector // self.directory_entry_size
 
 def ceildiv(a, b):
     return -(a // -b)
@@ -67,6 +76,9 @@ class FilesystemFatalError(Exception):
 class RecoverableCorruption(Exception):
     pass
 
+class DiskFullError(Exception):
+    pass
+
 @dataclass
 class DirectoryEntry:
     n:int
@@ -81,7 +93,7 @@ class DirectoryEntry:
     null:bool
 
     @classmethod
-    def fromraw(cls, n, raw):
+    def fromraw(cls, n, raw, *, geom):
         filename, extension, filetype, ascii_flag, first_granule, last_sector_bytes_used, _reserved = unpack("!8s3sBBBH16s", raw)
         null = set(raw) == { 0 }
         free = filename[0] == 0 or set(filename) == { 0xff }
@@ -102,9 +114,10 @@ class GranuleInfo:
     bytes_used:int          # number of bytes used in this granule
     last:bool               # True if this is the last sector number
     free:bool               # True if this granule is free
+    geom:Geometry
 
     @classmethod
-    def fromraw(cls, n, raw_value, last_sector_bytes_used):
+    def fromraw(cls, n, raw_value, last_sector_bytes_used, *, geom):
         if isinstance(raw_value, int):
             (b,) = bytes([raw_value])
         else:
@@ -114,19 +127,20 @@ class GranuleInfo:
                 raw_value=b,
                 n=n,
                 next_n=b,
-                sectors_used=sectors_per_granule,
-                bytes_used=sectors_per_granule * bytes_per_sector,
+                sectors_used=geom.sectors_per_granule,
+                bytes_used=geom.sectors_per_granule * geom.bytes_per_sector,
                 last=False,
                 free=False,
+                geom=geom
             )
         elif 0xc0 <= b <= 0xc9:
             sectors_used = b & 0xf
-            assert sectors_used <= sectors_per_granule, (n, raw_value, sectors_used)
+            assert sectors_used <= geom.sectors_per_granule, (n, raw_value, sectors_used)
             if sectors_used == 0:
                 assert last_sector_bytes_used == 0, last_sector_bytes_used
                 bytes_used = 0
             else:
-                bytes_used = (sectors_used - 1) * bytes_per_sector + last_sector_bytes_used
+                bytes_used = (sectors_used - 1) * geom.bytes_per_sector + last_sector_bytes_used
             return cls(
                 raw_value=b,
                 n=n,
@@ -135,6 +149,7 @@ class GranuleInfo:
                 bytes_used=bytes_used,
                 last=True,
                 free=False,
+                geom=geom,
             )
         elif b == 0xff:
             return cls(
@@ -145,11 +160,13 @@ class GranuleInfo:
                 bytes_used=0,
                 last=True,
                 free=True,
+                geom=geom,
             )
         else:
             raise RecoverableCorruption(f"granule #{n} corrupt: value 0x{b:02x} out of range")
 
 
+# for statvfs
 @dataclass
 class FilesystemStats:
     total_granule_count:int
@@ -159,13 +176,14 @@ class FilesystemStats:
     free_direntry_count:int
     used_direntry_count:int
     size_in_granules:int
-    sectors_per_track:int = sectors_per_track
-    sectors_per_granule:int = sectors_per_granule
-    bytes_per_sector:int = bytes_per_sector
+    sectors_per_track:int
+    sectors_per_granule:int
+    bytes_per_sector:int
 
 class RSDOSFilesystem:
     def __init__(self, io):
         self.io = io
+        self.geom = Geometry()
 
         # lock for single-threaded operations
         self.lock = threading.Lock()
@@ -189,37 +207,37 @@ class RSDOSFilesystem:
 
     def sector_count(self):
         n = self._getsize64()
-        extraneous_bytes = -(n % -bytes_per_sector)
+        extraneous_bytes = -(n % -self.geom.bytes_per_sector)
         if extraneous_bytes:
             warnings.warn(f"filesystem is not an even multiple of the sector size {extraneous_bytes:d} bytes will be ignored", OddSizeWarning)
-        n_sectors = n // bytes_per_sector
-        if n_sectors < (directory_track + 1) * sectors_per_track:
+        n_sectors = n // self.geom.bytes_per_sector
+        if n_sectors < (self.geom.directory_track + 1) * self.geom.sectors_per_track:
             raise FilesystemFatalError(f"filesystem is smaller than the minimum size")
 
         return n_sectors
 
     def granule_count(self):
-        s = self.sector_count() - sectors_per_track
-        extraneous_sectors = -(s % -sectors_per_granule)
+        s = self.sector_count() - self.geom.sectors_per_track
+        extraneous_sectors = -(s % -self.geom.sectors_per_granule)
         if extraneous_sectors:
             warnings.warn(f"filesystem is not an even multiple of the granule size {extraneous_sectors:d} sectors will be ignored", OddSizeWarning)
-        return s // sectors_per_granule
+        return s // self.geom.sectors_per_granule
 
     def sector_range_linear(self, n, sector_count=1):
         # This uses 0-based sector numbering
-        start = n * bytes_per_sector
-        end = start + bytes_per_sector * sector_count
+        start = n * self.geom.bytes_per_sector
+        end = start + self.geom.bytes_per_sector * sector_count
         return (start, end)
 
     def sector_range(self, cyl, secnr, sector_count=1):
         # This uses 1-based sector numbering (just like the on-disk format)
         if not 0 <= cyl:
             raise IndexError("cylinder number out of range")
-        if not 1 <= secnr <= sectors_per_track:
+        if not 1 <= secnr <= self.geom.sectors_per_track:
             raise IndexError("sector number out of range")
-        if not 0 <= sector_count <= sectors_per_track - (secnr-1):
+        if not 0 <= sector_count <= self.geom.sectors_per_track - (secnr-1):
             raise IndexError("sector count out of range")
-        return self.sector_range_linear(cyl * sectors_per_track + (secnr-1), sector_count)
+        return self.sector_range_linear(cyl * self.geom.sectors_per_track + (secnr-1), sector_count)
 
     def granule_range(self, granule):
         sec_start, sec_end = self.granule_range_sectors(granule)
@@ -228,29 +246,29 @@ class RSDOSFilesystem:
         return (start, end)
 
     def granule_range_sectors(self, granule):
-        sec_start = granule * sectors_per_granule
-        sec_end = (granule + 1) * sectors_per_granule
-        if sec_start >= directory_track * sectors_per_track:
+        sec_start = granule * self.geom.sectors_per_granule
+        sec_end = (granule + 1) * self.geom.sectors_per_granule
+        if sec_start >= self.geom.directory_track * self.geom.sectors_per_track:
             # Skip over the Directory track (track 17)
-            sec_start += sectors_per_track
-            sec_end += sectors_per_track
+            sec_start += self.geom.sectors_per_track
+            sec_end += self.geom.sectors_per_track
         return (sec_start, sec_end)
 
     def directory_range(self):
         # Track 17, sectors 3 - 11 (9 sectors)
-        return self.sector_range(directory_track, 3, 9)
+        return self.sector_range(self.geom.directory_track, 3, self.geom.directory_sectors)
 
     def directory_entry_range(self, n):
-        d_start, d_end = self.sector_range(directory_track, 3, 9)
-        dent_start = d_start + n * directory_entry_size
-        dent_end = dent_start + directory_entry_size
+        d_start, d_end = self.sector_range(self.geom.directory_track, 3, self.geom.directory_sectors)
+        dent_start = d_start + n * self.geom.directory_entry_size
+        dent_end = dent_start + self.geom.directory_entry_size
         if not d_start <= dent_start < d_end or not d_start <= dent_end <= d_end:
             raise IndexError("directory entry out of range")
         return (dent_start, dent_end)
 
     def granule_map_range(self):
-        start = self.sector_range(directory_track, 2)[0]
-        end = start + min(self.granule_count(), max_granules)
+        start = self.sector_range(self.geom.directory_track, 2)[0]
+        end = start + min(self.granule_count(), self.geom.max_granules)
         return (start, end)
 
     def pread(self, /, length, offset):
@@ -279,7 +297,7 @@ class RSDOSFilesystem:
         total_count = free_count = used_count = 0
         gmap = self.read_granule_map()
         for i, c in enumerate(gmap):
-            gi = GranuleInfo.fromraw(i, c, bytes_per_sector)
+            gi = GranuleInfo.fromraw(i, c, self.geom.bytes_per_sector, geom=self.geom)
             if gi.free:
                 free_count += 1
             else:
@@ -288,9 +306,9 @@ class RSDOSFilesystem:
         return (total_count, free_count, used_count)
 
     def iter_directory_entries(self, *, include_free=False):
-        for i in range(0, max_directory_entries):
+        for i in range(0, self.geom.max_directory_entries):
             raw = self.read_directory_entry(i)
-            dentry = DirectoryEntry.fromraw(i, raw)
+            dentry = DirectoryEntry.fromraw(i, raw, geom=self.geom)
             if dentry.null:     # HACK
                 continue
             if dentry.free and not include_free:
@@ -312,7 +330,7 @@ class RSDOSFilesystem:
         n = first_granule
         while True:
             raw_value = self.read_granule(n)
-            gi = GranuleInfo.fromraw(n, raw_value, last_sector_bytes_used)
+            gi = GranuleInfo.fromraw(n, raw_value, last_sector_bytes_used, geom=self.geom)
             yield gi
             seen.add(n)
             if gi.last:
@@ -325,12 +343,12 @@ class RSDOSFilesystem:
         return self.iter_granules(dentry.first_granule, dentry.last_sector_bytes_used)
 
     def compute_file_offset(self, dentry, offset):
-        granule_offset, x = divmod(offset, bytes_per_sector * sectors_per_granule)
-        sector_offset, byte_offset = divmod(x, bytes_per_sector)
+        granule_offset, x = divmod(offset, self.geom.bytes_per_sector * self.geom.sectors_per_granule)
+        sector_offset, byte_offset = divmod(x, self.geom.bytes_per_sector)
         return (granule_offset, sector_offset, byte_offset)
 
     def iter_file_contents(self, dentry, length=None, offset=0):
-        g0, b0 = divmod(offset, bytes_per_sector * sectors_per_granule)
+        g0, b0 = divmod(offset, self.geom.bytes_per_sector * self.geom.sectors_per_granule)
         bytes_remaining = length
         for gi in self.iter_dentry_granules(dentry):
             assert not gi.free
@@ -365,14 +383,14 @@ class RSDOSFilesystem:
         for gi in self.iter_dentry_granules(dentry):
             logical_size += gi.bytes_used
             allocated_granules += 1
-        allocated_sectors = allocated_granules * sectors_per_granule
-        return logical_size, bytes_per_sector, allocated_sectors
+        allocated_sectors = allocated_granules * self.geom.sectors_per_granule
+        return logical_size, self.geom.bytes_per_sector, allocated_sectors
 
     def get_sector_size(self):
-        return bytes_per_sector
+        return self.geom.bytes_per_sector
 
     def get_directory_size(self):
-        return directory_sectors * bytes_per_sector
+        return self.geom.directory_sectors * self.geom.bytes_per_sector
 
     def get_fs_stats(self):
         g_total, g_free, g_used = self.count_granules()
@@ -385,4 +403,7 @@ class RSDOSFilesystem:
             free_direntry_count=d_free,
             used_direntry_count=d_used,
             size_in_granules=ceildiv(self.sector_count(), sectors_per_granule),
+            sectors_per_track=self.geom.sectors_per_track,
+            sectors_per_granule=self.geom.sectors_per_granule,
+            bytes_per_sector=self.geom.bytes_per_sector,
         )
